647ef9dc8c2418494616da9c310bdc7d3dab9aff
Rodrigo Setti <rodrigosetti@gmail.com>
2011-09-15 21:47:14 -0300

    More Object-oriented like tokenizer

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,25 +1,20 @@
 #include "tokenizer.h"
+#include <sstream>
 
 using namespace std;
 
-Token::Token(const TokenType type,
-             const unsigned int line,
-             const unsigned int column,
-             const bool quoted,
-             const string symbolValue) {
+Token::Token( const TokenType type,
+              const unsigned int line,
+              const unsigned int column) {
     this->type = type;
-    this->line = line;
     this->column = column;
-    this->quoted = quoted;
-    this->symbolValue = symbolValue;
 }
 
 typedef enum { ST_COMMENT,
                ST_WHITESPACE,
                ST_SYMBOL,
                ST_OPEN,
-               ST_CLOSE,
-               ST_QUOTE} TokenizerState;
+               ST_CLOSE } TokenizerState;
 
 list<Token*> Token::tokenize(istream &stream) throw (SchemerException) {
 
@@ -30,10 +25,8 @@ list<Token*> Token::tokenize(istream &stream) throw (SchemerException) {
     unsigned int line = 1;
     unsigned int column = 0;
     unsigned int s = 0;
-    TokenType reservedSymbol;
-    char symbol_buffer[MAX_SYMBOL_LENGHT];
+    static char symbol_buffer[MAX_SYMBOL_LENGHT];
     string symbol_str;
-    bool quoted = false;
 
     while ( (next = stream.get()) != EOF) {
 
@@ -54,9 +47,6 @@ list<Token*> Token::tokenize(istream &stream) throw (SchemerException) {
                 cur_state = ST_OPEN;
                 break;
             case ')':
-                if (quoted && last_state != ST_SYMBOL) {
-                    throw SchemerException("Cannot quote before enclosing parenthesis", line, column);
-                }
                 cur_state = ST_CLOSE;
                 break;
             case '\n':
@@ -67,15 +57,6 @@ list<Token*> Token::tokenize(istream &stream) throw (SchemerException) {
             case ';':
                 cur_state = ST_COMMENT;
                 break;
-            case '\'':
-                if (last_state != ST_SYMBOL)
-                    quoted = false;
-                else {
-                    throw SchemerException("Illegal char \"'\" in token", line, column);
-                }
-                cur_state = ST_QUOTE;
-                quoted = true;
-                break;
             default:
                 cur_state = ST_SYMBOL;
         }
@@ -84,18 +65,19 @@ list<Token*> Token::tokenize(istream &stream) throw (SchemerException) {
         if (last_state == ST_SYMBOL && cur_state != ST_SYMBOL) {
             symbol_str = string(symbol_buffer, s);
 
-            if (!quoted)
-                reservedSymbol = isReservedWord(symbol_str);
-
-            if (!quoted && reservedSymbol != TOK_SYMBOL) {
-                tokens.push_back(new Token(reservedSymbol, line, column));
+            if (ReservedWordToken::match(symbol_str)) {
+                tokens.push_back(new ReservedWordToken(symbol_str, line, column));
+            }
+            else if (FloatToken::match(symbol_str)) {
+                tokens.push_back(new FloatToken(symbol_str, line, column));
+            }
+            else if (IntToken::match(symbol_str)) {
+                tokens.push_back(new IntToken(symbol_str, line, column));
             }
             else {
-                tokens.push_back(new Token(TOK_SYMBOL, line, column, quoted, symbol_str));
+                tokens.push_back(new SymbolToken(symbol_str, line, column));
             }
-
-            quoted = false;
-            s = 0;
+            s = 0; // start counting again
         }
         else if (cur_state == ST_SYMBOL) {
 
@@ -108,11 +90,10 @@ list<Token*> Token::tokenize(istream &stream) throw (SchemerException) {
 
         /* adds apropriate token for opening or closing parenthesis */
         if (cur_state == ST_OPEN) {
-            tokens.push_back(new Token(TOK_OPEN, line, column, quoted));
-            quoted = false;
+            tokens.push_back(new Token(TOK_OPEN, line, column));
         }
         else if (cur_state == ST_CLOSE) {
-            tokens.push_back(new Token(TOK_CLOSE, line, column, false));
+            tokens.push_back(new Token(TOK_CLOSE, line, column));
         }
 
         last_state = cur_state;
@@ -129,38 +110,122 @@ void Token::destroy(list<Token*> &tokens) {
     }
 }
 
-void Token::print(ostream &output) {
+/*************************************/
 
-    switch (type) {
-        case TOK_SYMBOL:
-            output << symbolValue;
-            break;
+SymbolToken::SymbolToken (const string &symbol,
+                          const unsigned int line,
+                          const unsigned int column) :
+    Token(TOK_SYMBOL, line, column) {
+    this->symbolValue = string(symbol);
+}
+
+/*************************************/
+
+ReservedWordToken::ReservedWordToken (const string &symbol,
+                                      const unsigned int line,
+                                      const unsigned int column) :
+    Token(TOK_RESERVED, line, column) {
+
+    if (symbol == "lambda") reservedWord = RES_LAMBDA;
+    else if (symbol == "define") reservedWord = RES_DEFINE;
+    else if (symbol == "if") reservedWord = RES_IF;
+    else if (symbol == "begin") reservedWord = RES_BEGIN;
+    else
+        throw SchemerException("Bad token allocation (reserved word)", line, column);
+}
+
+bool ReservedWordToken::match(const string &symbol) {
+
+    if (symbol == "lambda") return true;
+    else if (symbol == "define") return true;
+    else if (symbol == "if") return true;
+    else if (symbol == "begin") return true;
+    else return false;
+}
+
+/*************************************/
+
+FloatToken::FloatToken (const string &symbol,
+                        const unsigned int line,
+                        const unsigned int column) :
+    Token(TOK_FLOAT, line, column) {
+
+    istringstream test(symbol);
+    test >> floatValue;
+}
+
+bool FloatToken::match(const string &symbol) {
+    long double f;
+    istringstream test(symbol);
+    return (test >> f);
+}
+
+/*************************************/
+
+IntToken::IntToken (const string &symbol,
+                        const unsigned int line,
+                        const unsigned int column) :
+    Token(TOK_INT, line, column) {
+
+    istringstream test(symbol);
+    test >> intValue;
+}
+
+bool IntToken::match(const string &symbol) {
+    long int f;
+    istringstream test(symbol);
+    return (test >> f);
+}
+
+/****************************************/
+
+ostream& operator << (ostream &output, const Token &token) {
+
+    switch (token.type) {
         case TOK_OPEN:
             output << '(';
             break;
         case TOK_CLOSE:
             output << ')';
             break;
-        case TOK_DEFINE:
+    }
+
+    return output;
+}
+
+ostream& operator << (ostream &output, const SymbolToken &token) {
+
+    output << token.symbolValue;
+    return output;
+}
+
+ostream& operator << (ostream &output, const ReservedWordToken &token) {
+    switch (token.reservedWord) {
+        case RES_DEFINE:
             output << "DEFINE";
             break;
-        case TOK_BEGIN:
+        case RES_BEGIN:
             output << "BEGIN";
             break;
-        case TOK_IF:
+        case RES_IF:
             output << "IF";
             break;
-        case TOK_LAMBDA:
+        case RES_LAMBDA:
             output << "LAMBDA";
             break;
     }
+    return output;
 }
 
-TokenType Token::isReservedWord(const string &symbol) {
-
-    if (symbol == "lambda") return TOK_LAMBDA;
-    else if (symbol == "define") return TOK_DEFINE;
-    else if (symbol == "if") return TOK_IF;
-    else if (symbol == "begin") return TOK_BEGIN;
-    else return TOK_SYMBOL;
+ostream& operator << (ostream &output, const FloatToken &token) {
+    output << token.floatValue;
+    return output;
 }
+
+ostream& operator << (ostream &output, const IntToken &token) {
+    output << token.intValue;
+    return output;
+}
+
+
+
--- tokenizer.h
+++ tokenizer.h
@@ -1,70 +1,98 @@
 #ifndef __TOKENIZER_H__
 #define __TOKENIZER_H__
 
+#include "globals.h"
 #include <istream>
 #include <ostream>
-#include <string>
 #include <list>
 
 #define MAX_SYMBOL_LENGHT 128
 
 typedef enum { TOK_SYMBOL,
+               TOK_RESERVED,
                TOK_OPEN,
-               TOK_BEGIN,
-               TOK_LAMBDA,
-               TOK_DEFINE,
-               TOK_IF,
+               TOK_FLOAT,
+               TOK_INT,
                TOK_CLOSE } TokenType;
 
-class SchemerException {
+typedef enum { RES_LAMBDA,
+               RES_DEFINE,
+               RES_IF,
+               RES_BEGIN } ReservedWordType;
 
-    public:
-        unsigned int line;
-        unsigned int column;
-        std::string errorMessage;
-
-        SchemerException(const std::string errorMessage,
-                         const unsigned int line = 0,
-                         const unsigned int column = 0) {
-            this->errorMessage = errorMessage;
-            this->line = line;
-            this->line = column;
-        }
-
-        SchemerException(const char *errorMessage,
-                         const unsigned int line = 0,
-                         const unsigned int column = 0) {
-            this->errorMessage = std::string(errorMessage);
-            this->line = line;
-            this->line = column;
-        }
-};
 
 class Token {
 
     public:
 
-        std::string symbolValue;
         TokenType type;
         unsigned int line;
         unsigned int column;
-        bool quoted;
 
         Token ( const TokenType type,
                 const unsigned int line,
-                const unsigned int column,
-                bool quoted=false,
-                const std::string symbolValue = std::string());
-
-        void print (std::ostream &output);
+                const unsigned int column);
 
         static std::list<Token*> tokenize (std::istream &stream) throw (SchemerException);
         static void destroy(std::list<Token*> &tokens);
-
-    private:
-
-        static TokenType isReservedWord(const std::string &symbol);
 };
+
+class SymbolToken : public Token {
+
+    public:
+
+        std::string symbolValue;
+
+        SymbolToken (const std::string &symbol,
+                     const unsigned int line,
+                     const unsigned int column);
+
+};
+
+class ReservedWordToken : public Token {
+
+    public:
+
+        ReservedWordType reservedWord;
+
+        ReservedWordToken (const std::string &symbol,
+                           const unsigned int line,
+                           const unsigned int column);
+
+        static bool match(const std::string &symbol);
+};
+
+class FloatToken : public Token {
+
+    public:
+
+        long double floatValue;
+
+        FloatToken (const std::string &symbol,
+                    const unsigned int line,
+                    const unsigned int column);
+
+        static bool match(const std::string &symbol);
+};
+
+class IntToken : public Token {
+
+    public:
+
+        long int intValue;
+
+        IntToken (const std::string &symbol,
+                  const unsigned int line,
+                  const unsigned int column);
+
+        static bool match(const std::string &symbol);
+};
+
+std::ostream & operator << (std::ostream &output, const Token &token);
+std::ostream & operator << (std::ostream &output, const SymbolToken &token);
+std::ostream & operator << (std::ostream &output, const ReservedWordToken &token);
+std::ostream & operator << (std::ostream &output, const FloatToken &token);
+std::ostream & operator << (std::ostream &output, const IntToken &token);
 
 #endif
 
