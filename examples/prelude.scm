8e3b58e91f245371cf33c5cd31b0c9f02866334c
Rodrigo Setti <rodrigosetti@gmail.com>
2011-10-01 14:39:05 -0300

    builds a single prelude.scm file with essential scheme stuff

--- examples/prelude.scm
+++ examples/prelude.scm
@@ -1,13 +1,47 @@
-;; This file provides some basic constructions on lists
-;; and operations in such
+;; This file provides some essencial scheme stuff to enrich
+;; interpreter capabilities
 
 (begin
 
     (define empty nil)
 
+    (define average
+        (lambda (x y)
+                (/ (+ x y) 2)))
+
+    (define neg
+        (lambda (x)
+            (* -1 x)))
+
     (define -
         (lambda (x y)
             (+ x (neg y))))
+
+    (define abs
+        (lambda (x)
+            (if (< x 0)
+                (neg x)
+                x)))
+
+    (define square
+        (lambda (x)
+            (* x x)))
+
+    (define ^
+        (lambda (r e)
+                (if (= 0 e)
+                    1
+                    (* r (^ r (- e 1))))))
+
+    (define sqrt
+        (lambda (x)
+            (begin
+                (define guess
+                    (lambda (x g)
+                            (if (< (abs (- (square g) x)) 0.001)
+                                g
+                                (guess x (average g (/ x g))))))
+                (guess x (/ x 2)))))
 
     (define not
         (lambda (x)
