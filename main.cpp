4ae0620ece575da0d62bbb78ad11403ebe689981
Rodrigo Setti <rodrigosetti@gmail.com>
2011-10-01 14:36:14 -0300

    accept scheme file via command line parameters

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include "schemer.h"
 #include <iostream>
+#include <fstream>
 #include <sstream>
 
 using namespace std;
 
-int main() {
+int main(int argc, char **argv) {
     stringstream stream (stringstream::in | stringstream::out);
+    ifstream file;
     string line;
     list<Token*> tokens;
     Expression* expression;
     Environment *globalEnvironment = getGlobalEnvironment();
 
+    /* For each file given as parameter, open and evaluate it */
+    for (int c = 1; c < argc; c++) {
+        file.open(argv[c], ifstream::in);
+
+        tokens = Token::tokenize(file);
+        expression = Expression::parse(tokens);
+
+        expression->evaluate( globalEnvironment );
+    }
+
+    /* Eval print loop */
     while (true) {
 
         stream.clear();
@@ -21,6 +34,7 @@ int main() {
 
         stream.flush();
 
+        /* if end of user input: exit */
         if (cin.eof()) {
             break;
         }
