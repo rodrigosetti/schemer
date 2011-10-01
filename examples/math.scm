a83062c8518f226647d33fbf957b017cc919e715
Rodrigo Setti <rodrigosetti@gmail.com>
2011-10-01 00:04:36 -0300

    implements if and - function

--- examples/math.scm
+++ examples/math.scm
@@ -1,11 +1,22 @@
 (begin
+
+    (define if
+        (lambda (condition conseq otherwise)
+            (cond
+                (condition conseq)
+                (#t otherwise))))
+
     (define average
         (lambda (x y)
                 (/ (+ x y) 2)))
 
     (define neg
         (lambda (x)
-            (- 0 x)))
+            (* -1 x)))
+
+    (define -
+        (lambda (x y)
+            (+ x (neg y))))
 
     (define abs
         (lambda (x)
